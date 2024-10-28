7a8e7352df02a06f3bc7a7149ab18aae44917051
Alessandro Astarita <aleast@caprionline.it>
2024-10-28 17:15:48 +0000

    Aggiunta possibilita' di caricare foto da s3

--- src/load.js
+++ src/load.js
@@ -1,12 +1,13 @@
 import fs from 'fs';
 import http from 'http';
 import https from 'https';
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 
 export const loadImage = async (path, options) => {
   const isRemote = path.startsWith('/http:') || path.startsWith('/https:');
 
   if (isRemote) {
-    const remoteImageUrl = path.substr(1);
+    const remoteImageUrl = path.substring(1);
     const protocol = remoteImageUrl.startsWith('https') ? https : http;
 
     return new Promise((resolve, reject) => {
@@ -28,6 +29,37 @@ export const loadImage = async (path, options) => {
         reject(err);
       });
     });
+  } else if (options.s3 && options.s3.accessKeyId && options.s3.secretAccessKey && options.s3.bucketName) {
+    const s3Client = new S3Client({
+      region: options.s3.region,
+      credentials: {
+        accessKeyId: options.s3.accessKeyId,
+        secretAccessKey: options.s3.secretAccessKey,
+      },
+    });
+
+    const params = {
+      Bucket: options.s3.bucketName,
+      Key: path.startsWith('/') ? path.substring(1) : path
+    };
+
+    try {
+      const command = new GetObjectCommand(params);
+      const data = await s3Client.send(command);
+
+      const streamToBuffer = async (stream) => {
+        return new Promise((resolve, reject) => {
+          const chunks = [];
+          stream.on('data', (chunk) => chunks.push(chunk));
+          stream.on('end', () => resolve(Buffer.concat(chunks)));
+          stream.on('error', (err) => reject(err));
+        });
+      };
+
+      return await streamToBuffer(data.Body);
+    } catch (err) {
+      throw err;
+    }
   } else {
     return fs.promises.readFile(options.baseDir + path);
   }
